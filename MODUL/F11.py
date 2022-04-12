8f61668d1dd3ab1a4c38c9cec923510882515a41
Muhammad Fadhil Amri <69853444+Mehmed13@users.noreply.github.com>
2022-04-12 21:47:38 +0700

    Update F11.py

--- MODUL/F11.py
+++ MODUL/F11.py
@@ -5,7 +5,7 @@ def search(datagame, parameter, isi):
         datasearch = [['d' for i in range(banyak_kolom)]for j in range(banyak_baris-1)]
         for i in range(1,banyak_baris):
             datasearch[i-1] = datagame[i] #menyalin datagame ke datasearch tanpa header
-
+    
     else:
         #penentuan kolom yang akan digunakan berdasarkan jenis parameter 
         if parameter == "id":
@@ -32,70 +32,73 @@ def search(datagame, parameter, isi):
                 indeks += 1
     return datasearch
 
-
-baca = open("game.csv","r") 
-listgame = baca.readlines()        #Penyimpanan isi file pada variabel
-banyak_baris = hitung_baris(listgame)
-banyak_kolom = hitung_kolom(listgame)
-datagame = [["d" for i in range(banyak_kolom)] for j in range(banyak_baris)] 
-datagame = pecah_csv(listgame, datagame)
-# Data Kolom
-# ID | NAMA | Harga | Kategori | Tahun Rilis | Stok
-baca.close()
-
-
-print(">>> search_game_at_store")
-#Input parameter
-id_game = input("Masukkan ID Game: ")
-nama_game = input("Masukkan Nama Game: ")
-harga_game = input("Masukkan Harga Game: ")
-kategori_game = input("Masukkan Kategori Game: ")
-tahunrilis_game = input("Masukkkan Tahun Rilis Game: ")
-
-#Searching
-
-#Mengambil data hasil search setiap parameter
-datasearch_id = search(datagame,"id",id_game)
-datasearch_nama = search(datagame,"nama",nama_game)
-datasearch_harga = search(datagame,"harga", harga_game)
-datasearch_kategori = search(datagame, "kategori", kategori_game)
-datasearch_tahunrilis = search(datagame, "tahunrilis", tahunrilis_game)
-
-#Membuat matriks datasearch_final
-list_banyakbaris = [0 for i in range(5)] #inisiasi list untuk menampung banyak baris search masing-masing parameter
-list_banyakbaris[0] = hitung_baris(datasearch_id)
-list_banyakbaris[1] = hitung_baris(datasearch_nama)
-list_banyakbaris[2] = hitung_baris(datasearch_harga)
-list_banyakbaris[3] = hitung_baris(datasearch_kategori)
-list_banyakbaris[4] = hitung_baris(datasearch_tahunrilis) 
-
-banyakbaris_final = 0
-for i in range(list_banyakbaris[0]): #penghitungan banyak baris untuk datasearchfinal
-    for j in range(list_banyakbaris[1]):
-        for k in range(list_banyakbaris[2]):
-            for l in range(list_banyakbaris[3]):
-                for m in range(list_banyakbaris[4]):
-                    if datasearch_id[i] == datasearch_nama[j] ==datasearch_harga[k] == datasearch_kategori[l] == datasearch_tahunrilis[m]:
-                        banyakbaris_final +=1
-    
-datasearch_final = [['d' for i in range(banyak_kolom)]for j in range(banyakbaris_final+1)] #+1 untuk header
-
-datasearch_final[0] = datagame[0] #header
-
-indeks = 1
-for i in range(list_banyakbaris[0]): #penghitungan banyak baris untuk datasearchfinal
-    for j in range(list_banyakbaris[1]):
-        for k in range(list_banyakbaris[2]):
-            for l in range(list_banyakbaris[3]):
-                for m in range(list_banyakbaris[4]):
-                    if datasearch_id[i] == datasearch_nama[j] ==datasearch_harga[k] == datasearch_kategori[l] == datasearch_tahunrilis[m]:
-                        datasearch_final[indeks] = datasearch_id[i] #bisa pilih salah satu datasearch
-                        indeks +=1 
+def carigame_toko():
+    baca = open("game.csv","r") 
+    listgame = baca.readlines()        #Penyimpanan isi file pada variabel
+    banyak_baris = hitung_baris(listgame)
+    banyak_kolom = hitung_kolom(listgame)
+    datagame = [["d" for i in range(banyak_kolom)] for j in range(banyak_baris)] 
+    datagame = pecah_csv(listgame, datagame)
+    # Data Kolom
+    # ID | NAMA | Harga | Kategori | Tahun Rilis | Stok
+    baca.close()
 
 
-print("Daftar game pada inventory yang memenuhi kriteria: ")
-if banyakbaris_final == 0:
-    print("Tidak ada game pada toko yang memenuhi kriteria")
-else:
-    cetaklist(datasearch_final,banyak_kolom,banyakbaris_final+1)                     
-print(">>>")
+    print(">>> search_game_at_store")
+    #Input parameter
+    id_game = input("Masukkan ID Game: ")
+    nama_game = input("Masukkan Nama Game: ")
+    harga_game = input("Masukkan Harga Game: ")
+    kategori_game = input("Masukkan Kategori Game: ")
+    tahunrilis_game = input("Masukkkan Tahun Rilis Game: ")
+
+    #Searching
+
+    #Mengambil data hasil search setiap parameter
+    datasearch_id = search(datagame,"id",id_game)
+    datasearch_nama = search(datagame,"nama",nama_game)
+    datasearch_harga = search(datagame,"harga", harga_game)
+    datasearch_kategori = search(datagame, "kategori", kategori_game)
+    datasearch_tahunrilis = search(datagame, "tahunrilis", tahunrilis_game)
+
+    #Membuat matriks datasearch_final
+    list_banyakbaris = [0 for i in range(5)] #inisiasi list untuk menampung banyak baris search masing-masing parameter
+    list_banyakbaris[0] = hitung_baris(datasearch_id)
+    list_banyakbaris[1] = hitung_baris(datasearch_nama)
+    list_banyakbaris[2] = hitung_baris(datasearch_harga)
+    list_banyakbaris[3] = hitung_baris(datasearch_kategori)
+    list_banyakbaris[4] = hitung_baris(datasearch_tahunrilis) 
+
+    banyakbaris_final = 0
+    for i in range(list_banyakbaris[0]): #penghitungan banyak baris untuk datasearchfinal
+        for j in range(list_banyakbaris[1]):
+            for k in range(list_banyakbaris[2]):
+                for l in range(list_banyakbaris[3]):
+                    for m in range(list_banyakbaris[4]):
+                        if datasearch_id[i] == datasearch_nama[j] ==datasearch_harga[k] == datasearch_kategori[l] == datasearch_tahunrilis[m]:
+                            banyakbaris_final +=1
+        
+    datasearch_final = [['d' for i in range(banyak_kolom)]for j in range(banyakbaris_final+1)] #+1 untuk header
+
+    datasearch_final[0] = datagame[0] #header
+
+    indeks = 1
+    for i in range(list_banyakbaris[0]): #penghitungan banyak baris untuk datasearchfinal
+        for j in range(list_banyakbaris[1]):
+            for k in range(list_banyakbaris[2]):
+                for l in range(list_banyakbaris[3]):
+                    for m in range(list_banyakbaris[4]):
+                        if datasearch_id[i] == datasearch_nama[j] ==datasearch_harga[k] == datasearch_kategori[l] == datasearch_tahunrilis[m]:
+                            datasearch_final[indeks] = datasearch_id[i] #bisa pilih salah satu datasearch
+                            indeks +=1 
+
+
+    print("Daftar game pada inventory yang memenuhi kriteria: ")
+    if banyakbaris_final == 0:
+        print("Tidak ada game pada toko yang memenuhi kriteria")
+    else:
+        cetaklist(datasearch_final,banyak_kolom,banyakbaris_final+1)                     
+    print(">>>")
+
+if __name__ == "__main__":
+    carigame_toko()

7a04533ef9b1208e62240de750b4c22e04bf0892
sherraaa <102856322+sherraaa@users.noreply.github.com>
2022-04-13 20:06:30 +0700

    f13 klar finally
    b(￣▽￣)d klar bosku

--- MODUL/f13riwayatbeli.py
+++ MODUL/f13riwayatbeli.py
@@ -19,15 +19,15 @@ for i in range(banyak_baris):
     if datariwayat[i][3] == user_id:
         banyak_game+=1
 
-# pengambilan data riwayat yang sesuai dengan user
-datariwayatuser = [["d" for i in range(banyak_kolom-1)] for j in range(banyak_game)]
+# inisialisasi matriks data riwayat yang sesuai dengan user
+datariwayatuser = [["d" for i in range(banyak_kolom-1)] for j in range(banyak_game+1)]
 
 # ambil data riwayat yang sesuai dengan user id
-indeks = 2
-for i in range(banyak_game):
+indeks = 1
+for i in range(banyak_baris):
     datariwayatuser[0] = datariwayat[0] # header
-    for j in range(banyak_kolom-1):
-        if datariwayat[i][3] == user_id:
-            datariwayatuser[indeks] = datariwayat[i]
+    if datariwayat[i][3] == user_id:
+        datariwayatuser[indeks] = datariwayat[i]
+        indeks+=1
 
-cetaklist(datariwayatuser,banyak_kolom-1,banyak_game)
+cetaklist(datariwayatuser,banyak_kolom-1,banyak_game+1)

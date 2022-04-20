c2581abf0920a071f237fbbf9ebae0c10d6bf10a
Muhammad Fadhil Amri <69853444+Mehmed13@users.noreply.github.com>
2022-04-20 18:11:40 +0700

    Update F09.py
    Penyesuaian format output menggunakan fungsi cetakdata

--- BNMO/modul/F09.py
+++ BNMO/modul/F09.py
@@ -22,27 +22,19 @@ def list_game(datagame, datakepemilikan, users_id):
     global lihatgame
     gameid_user(datakepemilikan,users_id)
     banyak_baris_game = length(datagame)
+    banyak_kolom = length(datagame[0])
     if (banyak_game == 0):
         print("Maaf, kamu belum membeli game. Ketik perintah beli_game untuk beli.")
     else:
-        lihatgame = [["" for i in range(6)] for j in range(banyak_game)]
+        lihatgame = [["" for i in range(banyak_kolom)] for j in range(banyak_game+1)]
             # Data Kolom
-            # Nomor | ID Game | Nama | Kategori | Tahun | Harga
-        nomor = 1
-        for i in range(banyak_game):
-            lihatgame[i][0] = str(nomor)+"."
-            nomor = nomor + 1
-
-        daftar = 0
-        for i in range(banyak_game):
+            #ID Game | Nama | Kategori | Tahun | Harga
+        lihatgame[0] = datagame[0] #header
+        daftar = 1
+        for i in range(1, banyak_game+1):
             for j in range(banyak_baris_game):
-                if (datagame[j][0] == list_gameid[i]):
-                    lihatgame[daftar][1] = datagame[j][0]
-                    lihatgame[daftar][2] = datagame[j][1]
-                    lihatgame[daftar][3] = datagame[j][3]
-                    lihatgame[daftar][4] = datagame[j][4]
-                    lihatgame[daftar][5] = datagame[j][2]
+                if (datagame[j][0] == list_gameid[i-1]):
+                    lihatgame[daftar]= datagame[j]
                     daftar = daftar + 1
         print("Daftar Game:")
-        for i in range(banyak_game):
-            print(lihatgame[i][0], lihatgame[i][1], "|", lihatgame[i][2], "|", lihatgame[i][3], "|", lihatgame[i][4], "|", lihatgame[i][5])
+        cetakdata(lihatgame, banyak_kolom,banyak_game+1)

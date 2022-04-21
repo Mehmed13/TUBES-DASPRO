33254e7f08c50dbee7283e93ce8f26e12043cbbf
mrFrost1694 <98254048+mrFrost1694@users.noreply.github.com>
2022-04-21 23:14:54 +0700

    Update F03.py

--- MODUL/F03.py
+++ MODUL/F03.py
@@ -1,25 +1,22 @@
-from matriks import * # manggil semua function yang ada di file matriks
+from unicodedata import name
+from modul.pecah import * 
 
-def login():
-    datauser = load("users.csv") # nge load file users.csv
-
+def login(datauser):
     username = input("Masukkan username : ")
     password = input("Masukkan password : ")
-    banyak_baris = hitung_baris(datauser)
-
+    banyak_baris = length(datauser)
     login_success = False
     while(not login_success):
-        for i in range(banyak_baris):
-            if datauser[i][1] == username and datauser[1][3]:
-                login_success = Truename =username
-                name = username
-                pw = password
-            if not login_success :
-                print("Password atau username salah atau tidak ditemukan")
-                username = input ("Masukkan username : ")
-                password = input ("Masukkan password : ")
+        for i in range(1, banyak_baris):
+            if datauser[i][1] == username and datauser[i][3] == password:
+                login_success = True
+                user_id = datauser[i][0]
+                user_role = datauser[i][4]
+                break
+        if not login_success:
+            print("Password atau username salah atau tidak ditemukan")
+            username = input ("Masukkan username : ")
+            password = input ("Masukkan password : ")
 
-    print("Halo" + name + "! Selamat datang di Binomo")
-
-if __name__ == "__main__":
-    login()
+    print("Halo " + username + "! Selamat datang di Binomo")
+    return user_id, user_role

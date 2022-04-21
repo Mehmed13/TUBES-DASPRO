3387dfd92dcae81a9c8beb6944a04f689e98ea4e
Muhammad Fadhil Amri <69853444+Mehmed13@users.noreply.github.com>
2022-04-21 11:05:24 +0700

    Perbaikan F03

--- BNMO/modul/F03.py
+++ BNMO/modul/F03.py
@@ -1,4 +1,4 @@
-from unicodedata import name
+
 from modul.pecah import * # manggil semua function yang ada di file matriks
 
 def login(datauser):
@@ -11,11 +11,12 @@ def login(datauser):
             if datauser[i][1] == username and datauser[i][3] == password:
                 login_success = True
                 user_id = datauser[i][0]
+                role = datauser[i][4]
                 break
         if not login_success:
             print("Password atau username salah atau tidak ditemukan")
             username = input ("Masukkan username : ")
             password = input ("Masukkan password : ")
 
-    print("Halo" + username + "! Selamat datang di Binomo")
-    return user_id
+    print("Halo " + username + "! Selamat datang di Binomo")
+    return [user_id, role]

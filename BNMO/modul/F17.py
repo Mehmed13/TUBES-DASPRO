2ff3852d79c5387a74cd36b20df9fe09e69057eb
Muhammad Fadhil Amri <69853444+Mehmed13@users.noreply.github.com>
2022-04-23 14:00:48 +0700

    F17 Final

--- BNMO/modul/F17.py
+++ BNMO/modul/F17.py
@@ -1,10 +1,11 @@
-from F16 import *
+from modul.F16 import *
 
-answer = ""
-while answer != 'y' or answer != 'n' or answer != 'Y' or answer != 'N':
-    answer = input("Apakah anda mau melakukan penyimpanan file yang sudah diubah ? (y/n) : ")
-    if answer == 'Y' or answer == 'y':
-        save()
-        exit()
-    elif answer == 'N' or answer == 'n' :
-        exit()
+def func_exit(datagameUTAMA, datakepemilikanUTAMA, datariwayatUTAMA, datauserUTAMA):
+    answer = ""
+    while answer != 'y' or answer != 'n' or answer != 'Y' or answer != 'N':
+        answer = input("Apakah anda mau melakukan penyimpanan file yang sudah diubah ? (y/n) : ")
+        if answer == 'Y' or answer == 'y':
+            save(datagameUTAMA,datakepemilikanUTAMA,datariwayatUTAMA,datauserUTAMA)
+            exit()
+        elif answer == 'N' or answer == 'n' :
+            exit()

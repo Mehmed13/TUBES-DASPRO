fc88876c981d6efe3bf2eb848c41e16eba4900f0
Muhammad Fadhil Amri <69853444+Mehmed13@users.noreply.github.com>
2022-04-23 13:48:59 +0700

    F03 final

--- BNMO/modul/F03.py
+++ BNMO/modul/F03.py
@@ -1,5 +1,6 @@
-from unicodedata import name
-from modul.pecah import * 
+
+from modul.pecah import * # manggil semua function yang ada di file matriks
+from modul.cipher import*
 
 def login(datauser):
     username = input("Masukkan username : ")
@@ -8,10 +9,10 @@ def login(datauser):
     login_success = False
     while(not login_success):
         for i in range(1, banyak_baris):
-            if datauser[i][1] == username and datauser[i][3] == password:
+            if datauser[i][1] == username and  reverse_ciphered(datauser[i][3]) == password:
                 login_success = True
                 user_id = datauser[i][0]
-                user_role = datauser[i][4]
+                role = datauser[i][4]
                 break
         if not login_success:
             print("Password atau username salah atau tidak ditemukan")
@@ -19,4 +20,4 @@ def login(datauser):
             password = input ("Masukkan password : ")
 
     print("Halo " + username + "! Selamat datang di Binomo")
-    return user_id, user_role
+    return [user_id, role]

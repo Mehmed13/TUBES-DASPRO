5aa5e32a979a2315a42e6350894151b05107438a
Muhammad Fadhil Amri <69853444+Mehmed13@users.noreply.github.com>
2022-04-23 13:54:23 +0700

    F12 Final

--- BNMO/modul/F12.py
+++ BNMO/modul/F12.py
@@ -1,28 +1,30 @@
 from modul.pecah import *
 
-def topup(datauser, username, topup):
+def topup(datauser):
+    username = input("Masukkan username: ")
+    topup = int(input("Masukkan saldo: "))
     banyak_baris = length(datauser)
-    banyak_kolom = hitung_kolom(datauser)
-
-    # inisialisasi matriks data username yang sesuai dengan masukan (username yang mau ditop-up)
-    datauser_topup = [["d" for i in range(banyak_kolom)] for j in range(2)]
-
+    user_exist = False
     # ambil data user yang usernamenya sesuai dengan input
-    indeks = 1
+
     for i in range(banyak_baris):
-        datauser_topup[0] = datauser[0] # header
         if datauser[i][1] == username:
-            datauser_topup[indeks] = datauser[i]
-            indeks+=1
+            datauser_topup = datauser[i]
+            user_exist = True
+            indeks_user = i
+            break    
 
     # jika tidak ditemukan username yang sesuai, maka baris kedua hanya berisi "d"
-    if datauser_topup[1][1] == "d":
+    if not user_exist:
         print("Username "" + username + "" tidak ditemukan.")
     else: # jika username ada, proceed top-up
-        saldo = int(datauser_topup[1][5])
-        saldobaru = saldo + topup
+        saldo = int(datauser_topup[5])
+        saldo += topup #penambahan saldo
         if saldo < 0:
             print("Masukan tidak valid.")
         else: # saldo yang baru >= 0, top-up berhasil
-            datauser_topup[1][5] = str(saldobaru)
-            print(f"Top up berhasil. Saldo {username} bertambah menjadi {saldobaru}.")
+            datauser_topup[5] = str(saldo)
+            print(f"Top up berhasil. Saldo {username} bertambah menjadi {saldo}.")
+    #modif data
+    datauser[indeks_user] = datauser_topup
+    return datauser

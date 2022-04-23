daecdba56b6060b2408e3123b0fbb335fe1a5c92
Muhammad Fadhil Amri <69853444+Mehmed13@users.noreply.github.com>
2022-04-23 13:58:12 +0700

    F15 Final

--- BNMO/modul/F15.py
+++ BNMO/modul/F15.py
@@ -5,17 +5,16 @@ def load():
     parser.add_argument("foldername",type=str)
     try:
         args = parser.parse_args()
+        if checkFolder(args.foldername):
+            return args.foldername
+        else:
+            return ""
     except:
         print("Tidak ada nama folder yang diberikan!")
     
-    if checkFolder(args.foldername):
-        return args.foldername
-    else:
-        return ""
 
 def checkFolder(folderName): # membuat function check folder
     for root in os.walk(folderName): # mengecek list folder yang ada di dalam "folderName"
         if (root[0] == folderName): # Jika root index 0 (nama folder) sama dengan "folderName"
-    	    return True
+            return True
     return False
-
